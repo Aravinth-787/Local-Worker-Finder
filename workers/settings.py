8f9df0ca9026d84249808c245054cfe684798469
Aravinth <aravinthp02.2004@gmail.com>
2026-04-09 19:34:55 +0530

    Update TEMPLATES DIRS to include myapp Templates

--- workers/settings.py
+++ workers/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'workers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'myapp' / 'Templates],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
